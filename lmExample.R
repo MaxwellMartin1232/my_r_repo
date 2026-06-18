fd8445219e400829f003432daa840ec69f124d7f
Maxwell1232 <maxwell200731@gmail.com>
2026-06-17 22:06:39 -0600

    I just did this Ben was here

--- lmExample.R
+++ lmExample.R
@@ -9,4 +9,4 @@ abline(fit, col=2)
 co2 = read.csv("co2_mm_mlo.csv")
 plot(co2$decdate, co2$interpolated, type='l', main="Mauna Loa CO2",
      xlab="Time", ylab="ppm")
-lines(co2$decdate, co2$trend, col=2, lwd=2))
+lines(co2$decdate, co2$trend, col=2, lwd=2)
